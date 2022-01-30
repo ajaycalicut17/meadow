2e1daf5d4ff9c3caad84479ca163de6d30b61d3e
ajaycalicut17 <ajaycalicut17@gmail.com>
2022-01-30 14:40:30 +0530

    larastarter service provider added

---
+++ src/LarastarterServiceProvider.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Ajaycalicut17\Larastarter;
+
+use Illuminate\Support\ServiceProvider;
+
+class LarastarterServiceProvider extends ServiceProvider
+{
+    public function register()
+    {
+        # code...
+    }
+
+    public function boot()
+    {
+        # code...
+    }
+}
