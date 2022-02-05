a47c7a0997c8455d98fb36065b51c2225a27db24
ajaycalicut17 <ajaycalicut17@gmail.com>
2022-02-05 15:34:57 +0530

    install command

--- src/Console/Commands/InstallCommand.php
+++ src/Console/Commands/InstallCommand.php
@@ -42,17 +42,29 @@ class InstallCommand extends Command
         $this->callSilent('vendor:publish', ['--provider' => 'Laravel\Fortify\FortifyServiceProvider', '--force' => true]);
 
         // Fortify Provider
-        $this->installServiceProviderAfter('RouteServiceProvider', 'FortifyServiceProvider');
-    }
+        $this->replaceInFile('App\Providers\RouteServiceProvider::class,', '        App\Providers\FortifyServiceProvider::class,', config_path('app.php'));
 
-    protected function installServiceProviderAfter($after, $name)
+        // call method from the boot method
+        $search = "public function boot()" . PHP_EOL . "    {";
+        $replace = "        Fortify::loginView(function () {" . PHP_EOL . "            return view('auth.login');" . PHP_EOL . "        });" . 
+        PHP_EOL . "        Fortify::twoFactorChallengeView(function () {" . PHP_EOL . "            return view('auth.two-factor-challenge');" . PHP_EOL . "        });" . 
+        PHP_EOL . "        Fortify::registerView(function () {" . PHP_EOL . "            return view('auth.register');" . PHP_EOL . "        });" . 
+        PHP_EOL . "        Fortify::requestPasswordResetLinkView(function () {" . PHP_EOL . "            return view('auth.forgot-password');" . PHP_EOL . "        });" . 
+        PHP_EOL . "        Fortify::resetPasswordView(function (" . '$request' . ") {" . PHP_EOL . "            return view('auth.reset-password', ['request' => " . '$request' . "]);" . PHP_EOL . "        });" . 
+        PHP_EOL . "        Fortify::verifyEmailView(function () {" . PHP_EOL . "            return view('auth.verify-email');" . PHP_EOL . "        });" . 
+        PHP_EOL . "        Fortify::confirmPasswordView(function () {" . PHP_EOL . "            return view('auth.confirm-password');" . PHP_EOL . "        });";
+        $this->replaceInFile($search, $replace, app_path('Providers/FortifyServiceProvider.php'));
+    }
+    
+    protected function replaceInFile(string $search = '', string $replace = '', string $path = ''): bool
     {
-        if (!Str::contains($appConfig = file_get_contents(config_path('app.php')), 'App\\Providers\\' . $name . '::class')) {
-            file_put_contents(config_path('app.php'), str_replace(
-                'App\\Providers\\' . $after . '::class,',
-                'App\\Providers\\' . $after . '::class,' . PHP_EOL . '        App\\Providers\\' . $name . '::class,',
-                $appConfig
-            ));
+        if (file_exists($path)) {
+            $fileContents = file_get_contents($path);
+            if (!str_contains($fileContents, $replace)) {
+                file_put_contents($path, str_replace($search, ($search . PHP_EOL . $replace), $fileContents));
+            }
         }
+
+        return true;
     }
 }
