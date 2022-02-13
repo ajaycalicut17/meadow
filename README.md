89e48430de8c8a88c35c4aa19b9f6e5b15ec85ec
ajaycalicut17 <ajaycalicut17@gmail.com>
2022-02-13 14:53:06 +0530

    tailwindcss-multi-theme

--- README.md
+++ README.md
@@ -1,2 +1,28 @@
 # larastarter
 Laravel starter
+
+## Installation
+
+You can install the package via composer:
+
+``` bash
+composer require ajaycalicut17/larastarter --dev
+```
+
+Run artisan command for installation:
+
+``` bash
+php artisan larastarter:install
+```
+
+Run npm
+
+``` bash
+npm install && npm run dev
+```
+
+Database migration
+
+``` bash
+php artisan migrate
+```
--- src/Console/InstallCommand.php
+++ src/Console/InstallCommand.php
@@ -66,9 +66,12 @@ class InstallCommand extends Command
         // npm packages
         $this->updateNodePackages(function ($packages) {
             return [
-                'tailwindcss' => '^3.0.22',
+                'tailwindcss' => '^1.9.6',
+                'tailwindcss-multi-theme' => '^1.0.4',
+                '@tailwindcss/custom-forms' => '^0.2.1',
                 'postcss' => '^8.4.6',
                 'autoprefixer' => '^10.4.2',
+                'color' => '^4.2.1',
             ] + $packages;
         });
 
