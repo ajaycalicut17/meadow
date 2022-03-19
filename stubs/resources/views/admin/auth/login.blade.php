cc074d5464a48f774192b4eb87c636ba97c5afa8
ajaycalicut17 <ajaycalicut17@gmail.com>
2022-03-19 15:43:10 +0530

    login form

--- stubs/resources/views/admin/auth/login.blade.php
+++ stubs/resources/views/admin/auth/login.blade.php
@@ -10,16 +10,22 @@
             <h1 class="mb-4 text-xl font-semibold text-gray-700 dark:text-gray-200">
                 {{ __('Login') }}
             </h1>
-            <div class="block text-sm">
-                <x-forms.label name="{{ __('Email') }}" for="input-email" />
-                <x-inputs.text name="email" id="input-email" placeholder="{{ __('Email') }}" />
-            </div>
-            <div class="block mt-4 text-sm">
-                <x-forms.label name="{{ __('Password') }}" for="input-password" />
-                <x-inputs.password name="password" id="input-password" placeholder="***************" />
-            </div>
 
-            <x-buttons.link name="{{ __('Log in') }}" href="#" />
+            <form action="{{ route('login') }}" method="post">
+                @csrf
+                <div class="block text-sm">
+                    <x-forms.label name="{{ __('Email') }}" for="input-email" />
+                    <x-inputs.text name="email" id="input-email" placeholder="{{ __('Email') }}" />
+                    <x-forms.error name="email" />
+                </div>
+                <div class="block mt-4 text-sm">
+                    <x-forms.label name="{{ __('Password') }}" for="input-password" />
+                    <x-inputs.password name="password" id="input-password" placeholder="***************" />
+                    <x-forms.error name="password" />
+                </div>
+    
+                <x-buttons.submit name="{{ __('Log in') }}" />
+            </form>
 
             <hr class="my-8" />
 
---
+++ stubs/resources/views/components/forms/error.blade.php
@@ -0,0 +1,3 @@
+@error($name)
+    <span class="text-xs text-red-600 dark:text-red-400">{{ $message }}</span>
+@enderror
