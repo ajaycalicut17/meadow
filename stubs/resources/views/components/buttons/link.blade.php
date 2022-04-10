7baa31960385d693cacd464b57c8279aea836cf9
ajaycalicut17 <ajaycalicut17@gmail.com>
2022-04-10 17:25:36 +0530

    component button link

--- stubs/resources/views/components/buttons/link.blade.php
+++ stubs/resources/views/components/buttons/link.blade.php
@@ -1,6 +1,4 @@
-<a class="block w-full px-4 py-2 mt-4 text-sm font-medium leading-5 text-center text-white transition-colors duration-150 bg-purple-600 border border-transparent rounded-lg active:bg-purple-600 hover:bg-purple-700 focus:outline-none focus:shadow-outline-purple"
-    @isset($href) href="{{ $href }}" @endisset>
-    @isset($name)
-        {{ $name }}
-    @endisset
+<a
+    {{ $attributes->merge(['class' =>'block w-full px-4 py-2 mt-4 text-sm font-medium leading-5 text-center text-white transition-colors duration-150 bg-purple-600 border border-transparent rounded-lg active:bg-purple-600 hover:bg-purple-700 focus:outline-none focus:shadow-outline-purple']) }}>
+    {{ $slot }}
 </a>
