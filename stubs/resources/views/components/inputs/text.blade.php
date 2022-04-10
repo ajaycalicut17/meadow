5f989b86224e839e8175c76e036581d742e52de2
ajaycalicut17 <ajaycalicut17@gmail.com>
2022-04-10 20:20:07 +0530

    component inputs text

--- stubs/resources/views/components/inputs/text.blade.php
+++ stubs/resources/views/components/inputs/text.blade.php
@@ -1,7 +1,3 @@
-<input type="text" @isset($name) name="{{ $name }}" @endisset
-    @isset($id) id="{{ $id }}" @endisset
-    @error($name) class="block w-full mt-1 text-sm rounded-md shadow-sm border-red-600 dark:border-gray-600 dark:bg-gray-700 focus:border-red-400 focus:outline-none focus:shadow-outline-red dark:text-gray-300 dark:focus:shadow-outline-gray form-input" @enderror
-    class="block w-full mt-1 text-sm rounded-md shadow-sm border-gray-300 dark:border-gray-600 dark:bg-gray-700 focus:border-purple-400 focus:ring focus:ring-indigo-200 focus:ring-opacity-50 dark:text-gray-300 dark:focus:shadow-outline-gray form-input"
-    value="{{ isset($value) ? $value : old($name) }}"
-    @isset($placeholder) placeholder="{{ $placeholder }}" @endisset
-    @isset($readonly) readonly @endisset>
+<input type="text"
+    {{ $attributes->class(['block w-full mt-1 text-sm rounded-md shadow-sm dark:text-gray-300 dark:focus:shadow-outline-gray form-input','border-gray-300 dark:border-gray-600 dark:bg-gray-700 focus:border-purple-400 focus:ring focus:ring-indigo-200 focus:ring-opacity-50' => !$errors->has($name),'border-red-600 dark:border-gray-600 dark:bg-gray-700 focus:border-red-400 focus:outline-none focus:shadow-outline-red' => $errors->has($name)]) }}
+    value="{{ isset($value) ? $value : old($name) }}">
