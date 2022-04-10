f4b00e581f3926e221cc9931ec9319d842937e76
ajaycalicut17 <ajaycalicut17@gmail.com>
2022-04-10 16:55:00 +0530

    sidebar

--- stubs/resources/views/components/partials/sidebar.blade.php
+++ stubs/resources/views/components/partials/sidebar.blade.php
@@ -4,9 +4,7 @@
     </a>
     <ul class="mt-6">
         <li class="relative px-6 py-3">
-            <!-- Add this classes to an active anchor (a tag) -->
-            <!-- text-gray-800 dark:text-gray-100 -->
-            <a class="inline-flex items-center w-full text-sm font-semibold transition-colors duration-150 hover:text-gray-800 dark:hover:text-gray-200"
+            <a class="inline-flex items-center w-full text-sm font-semibold transition-colors duration-150 hover:text-gray-800 dark:hover:text-gray-200 @if (request()->is('home')) text-gray-800 dark:text-gray-100 @endif"
                 href="{{ url('home') }}">
                 <svg class="w-5 h-5" aria-hidden="true" fill="none" stroke-linecap="round" stroke-linejoin="round"
                     stroke-width="2" viewBox="0 0 24 24" stroke="currentColor">
@@ -17,9 +15,9 @@
                 <span class="ml-4">{{ __('Dashboard') }}</span>
             </a>
         </li>
-        <li class="relative px-6 py-3" x-data="{ isPagesMenuOpen: false }">
+        <li class="relative px-6 py-3" x-data="{ isPagesMenuOpen: @if (request()->is(['blank'])) true @else false @endif }">
             <button
-                class="inline-flex items-center justify-between w-full text-sm font-semibold transition-colors duration-150 hover:text-gray-800 dark:hover:text-gray-200"
+                class="inline-flex items-center justify-between w-full text-sm font-semibold transition-colors duration-150 hover:text-gray-800 dark:hover:text-gray-200 @if (request()->is(['blank'])) text-gray-800 dark:text-gray-100 @endif"
                 x-on:click="isPagesMenuOpen = ! isPagesMenuOpen" aria-haspopup="true">
                 <span class="inline-flex items-center">
                     <svg class="w-5 h-5" aria-hidden="true" fill="none" stroke-linecap="round"
@@ -30,11 +28,17 @@
                     </svg>
                     <span class="ml-4">{{ __('Pages') }}</span>
                 </span>
-                <svg class="w-4 h-4" aria-hidden="true" fill="currentColor" viewBox="0 0 20 20">
+                <svg x-show="! isPagesMenuOpen" class="w-4 h-4" viewBox="0 0 20 20" fill="currentColor"
+                    aria-hidden="true">
+                    <path fill-rule="evenodd"
+                        d="M12.707 5.293a1 1 0 010 1.414L9.414 10l3.293 3.293a1 1 0 01-1.414 1.414l-4-4a1 1 0 010-1.414l4-4a1 1 0 011.414 0z"
+                        clip-rule="evenodd"></path>
+                </svg>
+                <svg x-show="isPagesMenuOpen" class="w-4 h-4" viewBox="0 0 20 20" fill="currentColor"
+                    aria-hidden="true">
                     <path fill-rule="evenodd"
                         d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z"
-                        clip-rule="evenodd">
-                    </path>
+                        clip-rule="evenodd"></path>
                 </svg>
             </button>
             <template x-if="isPagesMenuOpen">
@@ -45,7 +49,8 @@
                     class="p-2 mt-2 space-y-2 overflow-hidden text-sm font-medium text-gray-500 rounded-md shadow-inner bg-gray-50 dark:text-gray-400 dark:bg-gray-900"
                     aria-label="submenu">
                     <li class="px-2 py-1 transition-colors duration-150 hover:text-gray-800 dark:hover:text-gray-200">
-                        <a class="w-full" href="./blank.html">{{ __('Blank') }}</a>
+                        <a class="w-full @if (request()->is('blank')) text-gray-800 dark:text-gray-100 @endif"
+                            href="./blank.html">{{ __('Blank') }}</a>
                     </li>
                 </ul>
             </template>
