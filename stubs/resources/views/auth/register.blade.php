ef06d4ee57c2a9d2cf79a93cb2473233e407d696
ajaycalicut17 <ajaycalicut17@gmail.com>
2022-04-23 13:43:16 +0530

    Registration

---
+++ stubs/resources/views/auth/register.blade.php
@@ -0,0 +1,45 @@
+<x-layouts.guest>
+    <div class="h-32 md:h-auto md:w-1/2">
+        <img aria-hidden="true" class="object-cover w-full h-full" src="{{ asset('img/office.jpeg') }}" alt="Office" />
+    </div>
+    <div class="flex items-center justify-center p-6 sm:p-12 md:w-1/2">
+        <div class="w-full">
+            <h1 class="mb-4 text-xl font-semibold text-gray-700 dark:text-gray-200">
+                {{ __('Register') }}
+            </h1>
+
+            <form action="{{ route('register') }}" method="post">
+                @csrf
+                <div class="block text-sm">
+                    <x-forms.label for="input-name">{{ __('Name') }}</x-forms.label>
+                    <x-inputs.text name="name" id="input-name" placeholder="{{ __('Name') }}" />
+                    <x-forms.error name="name" />
+                </div>
+                <div class="block mt-4 text-sm">
+                    <x-forms.label for="input-email">{{ __('Email') }}</x-forms.label>
+                    <x-inputs.text name="email" id="input-email" placeholder="{{ __('Email') }}" />
+                    <x-forms.error name="email" />
+                </div>
+                <div class="block mt-4 text-sm">
+                    <x-forms.label for="input-password">{{ __('Password') }}</x-forms.label>
+                    <x-inputs.password name="password" id="input-password" placeholder="***************" />
+                    <x-forms.error name="password" />
+                </div>
+                <div class="block mt-4 text-sm">
+                    <x-forms.label for="input-password-confirmation">{{ __('Confirm Password') }}</x-forms.label>
+                    <x-inputs.password name="password_confirmation" id="input-password-confirmation"
+                        placeholder="***************" />
+                    <x-forms.error name="password_confirmation" />
+                </div>
+
+                <x-buttons.submit>{{ __('Register') }}</x-buttons.submit>
+            </form>
+
+            <hr class="my-8" />
+
+            <p class="mt-1">
+                <x-partials.link href="{{ route('login') }}">{{ __('Back to login') }}</x-partials.link>
+            </p>
+        </div>
+    </div>
+</x-layouts.guest>
--- stubs/resources/views/components/partials/sidebar.blade.php
+++ stubs/resources/views/components/partials/sidebar.blade.php
@@ -5,7 +5,7 @@
     <ul class="mt-6">
         <li class="relative px-6 py-3">
             <a class="inline-flex items-center w-full text-sm font-semibold transition-colors duration-150 hover:text-gray-800 dark:hover:text-gray-200 @if (request()->routeIs('home')) text-gray-800 dark:text-gray-100 @endif"
-                href="{{ url('home') }}">
+                href="{{ route('home') }}">
                 <svg class="w-5 h-5" aria-hidden="true" fill="none" stroke-linecap="round" stroke-linejoin="round"
                     stroke-width="2" viewBox="0 0 24 24" stroke="currentColor">
                     <path
